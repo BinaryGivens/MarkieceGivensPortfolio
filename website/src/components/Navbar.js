602cf350e91a823bab6ec00a2ceda571c1be61dd
BinaryGivens <markiecegivens98@gmail.com>
2024-06-05 13:27:53 -0700

    Confirming github action success

--- website/src/components/Navbar.js
+++ website/src/components/Navbar.js
@@ -27,7 +27,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to='/' className="navbar-logo"onClick={closeMobileMenu}>
-            MARKIECE GAIaaaVENS <i className='fab fa-typo3' />
+            MARKIECE GIVENS <i className='fab fa-typo3' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
